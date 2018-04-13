d433b95d47e8f0582cbace1522f68fad38154424
Jan Galek <admin@gcore.cz>
2018-04-13 16:20:51 +0200

    Refactoring

--- src/Galek/Utils/Calendar/Helper.php
+++ src/Galek/Utils/Calendar/Helper.php
@@ -14,23 +14,41 @@ class Helper
 	public static function dateDifferenceTranslation(Calendar $date, Calendar $curDate, int $diff, array $local): string
 	{
 		if ($date >= $curDate) {
-			if ($diff === 0) {
-				return $local['today'];
-			} elseif ($diff === 1) {
-				return $local['tomorrow'];
-			} elseif ($diff === 2) {
-				return $local['afterTomorrow'];
-			} elseif ($diff < 5) {
-				return $local['after'] . ' ' . $diff . ' ' . $local['twoDays'];
-			}
-		} else {
-			if ($diff === 1) {
-				return $local['yesterday'];
-			} else {
-				return $local['before'] . ' ' . $diff . ' ' . $local['twoDays'];
-			}
+			return self::getDifferenceAfter($diff, $local);
 		}
 
+		return self::getDifferenceBefore($diff, $local);
+	}
+
+
+	private static function getDifferenceAfter(int $diff, array $local): string
+	{
+		if ($diff === 0) {
+			return $local['today'];
+		} elseif ($diff === 1) {
+			return $local['tomorrow'];
+		} elseif ($diff === 2) {
+			return $local['afterTomorrow'];
+		} elseif ($diff < 5) {
+			return $local['after'] . ' ' . $diff . ' ' . $local['twoDays'];
+		} else {
+			return self::getDifferenceDefault($diff, $local);
+		}
+	}
+
+
+	private static function getDifferenceBefore(int $diff, array $local): string
+	{
+		if ($diff === 1) {
+			return $local['yesterday'];
+		} else {
+			return $local['before'] . ' ' . $diff . ' ' . $local['twoDays'];
+		}
+	}
+
+
+	private static function getDifferenceDefault(int $diff, array $local): string
+	{
 		return $local['after'] . ' ' . $diff . ' ' . $local['fiveDays'];
 	}
 }
